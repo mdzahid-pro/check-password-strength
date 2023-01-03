27ea0fce25a0a58cfc006c989570ff78b6e941f0
Md Zahid <77210189+mdzahid-pro@users.noreply.github.com>
2023-01-03 16:16:42 +0600

    WIP 2023-01-03

---
+++ check-password-stength.php
@@ -0,0 +1,61 @@
+<?php
+	
+	
+
+public function checkPassword($pwd, &$errors) {
+    $errors_init = $errors;
+
+    if (strlen($pwd) < 8) {
+        $errors[] = "Password too short!";
+    }
+
+    if (!preg_match("#[0-9]+#", $pwd)) {
+        $errors[] = "Password must include at least one number!";
+    }
+
+    if (!preg_match("#[a-zA-Z]+#", $pwd)) {
+        $errors[] = "Password must include at least one letter!";
+    }     
+
+    return ($errors == $errors_init);
+}
+
+if(!empty($_POST["password"]) && ($_POST["password"] == $_POST["cpassword"])) {
+    $password = test_input($_POST["password"]);
+    $cpassword = test_input($_POST["cpassword"]);
+    if (strlen($_POST["password"]) <= '8') {
+        $passwordErr = "Your Password Must Contain At Least 8 Characters!";
+    }
+    elseif(!preg_match("#[0-9]+#",$password)) {
+        $passwordErr = "Your Password Must Contain At Least 1 Number!";
+    }
+    elseif(!preg_match("#[A-Z]+#",$password)) {
+        $passwordErr = "Your Password Must Contain At Least 1 Capital Letter!";
+    }
+    elseif(!preg_match("#[a-z]+#",$password)) {
+        $passwordErr = "Your Password Must Contain At Least 1 Lowercase Letter!";
+    }
+}elseif(!empty($_POST["password"])) {
+    $cpasswordErr = "Please Check You've Entered Or Confirmed Your Password!";
+} else {
+     $passwordErr = "Please enter password   ";
+}
+
+if(!empty($_POST["password"]) && ($_POST["password"] == $_POST["cpassword"])) {
+    $password = test_input($_POST["password"]);
+    $cpassword = test_input($_POST["cpassword"]);
+    if (strlen($_POST["password"]) <= 8) {
+        $passwordErr = "Your Password Must Contain At Least 8 Characters!";
+    }
+    elseif(!preg_match("#[0-9]+#",$password)) {
+        $passwordErr = "Your Password Must Contain At Least 1 Number!";
+    }
+    elseif(!preg_match("#[A-Z]+#",$password)) {
+        $passwordErr = "Your Password Must Contain At Least 1 Capital Letter!";
+    }
+    elseif(!preg_match("#[a-z]+#",$password)) {
+        $passwordErr = "Your Password Must Contain At Least 1 Lowercase Letter!";
+    } else {
+        $cpasswordErr = "Please Check You've Entered Or Confirmed Your Password!";
+    }
+}
